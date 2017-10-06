bcc9a3903baaeb75e0c1a57594d2235794ec89c3
ariffridasari <arif.fridasari@students.amikom.ac.id>
2017-10-06 10:28:34 +0700

    Add Login

--- mu-app/app/src/main/java/com/ariffridasari/mu_app/MainActivity.java
+++ mu-app/app/src/main/java/com/ariffridasari/mu_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.ariffridasari.mu_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,6 +56,43 @@ public class MainActivity extends AppCompatActivity {
         Log.d(msg, "The onDestroy() event");
     }
 
+    EditText editText1 ,editText2; //Deklarasi object dari class EdiText
+    String text1 ,text2; //Deklarasi object string
 
+
+
+    public void loginMasuk(View view) {
+        //Method onClick pada Button
+
+        editText1 = (EditText)findViewById(R.id.edittext_username);
+        editText2 = (EditText) findViewById(R.id.edittext_password);
+        text1 = editText1.getText().toString();
+        text2 = editText2.getText().toString();
+
+        //Kondisi jika username dan password benar maka akan menampilkan pesan text toast
+        //Login sukses dan masuk ke activity 2
+        if ((text1.contains("Username"))&&((text2.contains("Password")))) {
+            Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+
+        else if ((text1.matches("")||text2.matches("")))
+          /*
+          Atau jika input text 1 dan text 2 kosong
+           */
+        {
+            //Maka akan menampilkan pesan text toast
+            Toast.makeText(this, "Isikan Username dan Password", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else {
+            //jika kedua kondisi diatas tidak memenuhi
+
+            Toast.makeText(this, "Login Gagal /Username Password Salah", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
 }
